49454a668acdf213233fcdbf8b2715c5514419fb
Fofan228 <vovka.322@Mail.ru>
2022-12-19 15:17:32 +0500

    add footer and empty directories

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import logo from '@Assets/images/logo.png';
+import './footer.less';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                <div className="footer-logo">
+                    <img src={logo} className="logo" />
+                </div>
+                <div className="footer-info-begin">
+                    <dl className="footer-info-list-begin">
+                        <dt>О компании</dt>
+                        <dt>Вакансии</dt>
+                        <dt>Клиенту</dt>
+                        <dt>Франчайзинг</dt>
+                    </dl>
+                    <dl className="footer-info-list-middle">
+                        <dt>Доставка из китая</dt>
+                        <dt>Все услуги</dt>
+                        <dt>Акции</dt>
+                    </dl>
+                </div>
+                <div className="footer-info-begin">
+                    <dl className="footer-info-list-end">
+                        <dt>manager@uexpress.ru</dt>
+                        <dt>
+                            115221, г. Челябинск, ул.
+                            <br /> Северная, д. 29-в
+                        </dt>
+                        <dt>
+                            Мы работаем по 24 часа
+                            <br /> 5 дней в неделю
+                        </dt>
+                    </dl>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/pages/Main/main.jsx
+++ src/pages/Main/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from '@Components/Header/header.jsx';
+import Header from '@Components/Main-header/Header/header.jsx';
+import Footer from '@Components/Footer/footer';
 
 //import './main1.less';
 
@@ -7,6 +8,7 @@ const Main = () => (
     <div className="main-page">
         <div className="main-page__title">
             <Header />
+            <Footer />
         </div>
     </div>
 );
